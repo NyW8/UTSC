5ee7002bb86adefb7047b428717c946211511132
NyW8 <tobluee@gmail.com>
2019-07-31 09:06:18 -0400

    Added extra test

--- CSCB09/Assignment4tests/testAssignment4.sh
+++ CSCB09/Assignment4tests/testAssignment4.sh
@@ -6,24 +6,24 @@ gcc -g -w client.c -o client
 
 if [ ! -e lorem.txt -o ! -e gibberish.txt -o ! -e gibberish-large.txt ]; then echo "Please download ALL the testing files please!"; fi;
 
-echo Opening client port at 12121, PLEASE HAVE THIS SET UP BEFOREHAND
+echo Opening client port at 12121, PLEASE HAVE SERVER SET UP BEFOREHAND
 echo; echo TEST 1: lorem.txt
 ./client 127.0.0.1 12121 lorem.txt new_lorem.txt
 val1=$(diff lorem.txt new_lorem.txt)
 if [ "$val1" != "" ]; then echo Failed test 1: There were errors calling with lorem.txt;
-else echo Passed test 1; fi;
+else echo Passed test 1; rm new_lorem.txt; fi;
 
 echo; echo TEST 2: gibberish.txt
 ./client 127.0.0.1 12121 gibberish.txt new_gibb.txt
 val2=$(diff gibberish.txt new_gibb.txt)
 if [ "$val2" != "" ]; then echo Failed test 2: There were errors calling with gibberish.txt; 
-else echo Passed test 2; fi;
+else echo Passed test 2; rm new_gibb.txt; fi;
 
 echo; echo TEST 3: gibberish-large.txt
 ./client 127.0.0.1 12121 gibberish-large.txt new_gibb-large.txt
 val3=$(diff gibberish-large.txt new_gibb-large.txt)
 if [ "$val3" != "" ]; then echo Failed test 3: There were errors calling with gibberish-large.txt; 
-else echo Passed test 3; fi;
+else echo Passed test 3; rm new_gibb-large.txt fi;
 
 echo; echo TEST 4: SHOULD NOT ASSIGN THE REQUESTED ADDRESS:
 ./client 127.0.0.1 00000 lorem.txt new_lorem.txt
@@ -33,3 +33,8 @@ echo; echo TEST 4.5: SHOULD REFUSE THE CONNECTION:
 
 echo; echo TEST 5: SHOULD COMPLAIN ABOUT TOO FEW PARAMETERS
 ./client 127.0.0.1 12121 lorem.txt
+
+echo; echo TEST 6: NOT CREATING FILES IF SERVER DOES NOT SEND
+./client 127.0.0.1 12121 random000.ppp TSNE.txt
+if [ -e TNSE.txt ]; then echo Failed test 6: should not create new files if server could not send it!; rm TSNE.txt;
+else echo Passed test 6; fi;
