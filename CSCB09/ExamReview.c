e87b96be32d3b3777d861724fdb8a1da5ea1de72
NyW8 <tobluee@gmail.com>
2019-08-09 20:05:37 -0400

    Finished C programming review reminders

--- CSCB09/ExamReview.c
+++ CSCB09/ExamReview.c
@@ -1,4 +1,11 @@
+//Created by Nyah Way August 9th, 2019
+//Final exam review for CSCB09 Summer 2019
 
+
+
+
+
+//SYSTEM CALLS:
 //opens a path given with flags
 int open(const char *path, int flags/*, mode_t mode*/);
 /*	flags:
@@ -51,8 +58,75 @@ int select(int n, fd_set *readset, fd_set *writeset, fd_set *exceptset, struct t
 		if timeout == 0, returns answer immediately
 	*/
 
+//operations on fd_sets:
+void FD_ZERO(fd_set *s);		//sets all values in the set to 0
+void FD_SET(int fd, fd_set *s);	//add descriptor fd into set s
+void FD_CLR(int fd, fd_set *s);	//remove descriptor fd from set s
+int FD_ISSET(int fd, fd_set *s);//check if fd is part of set s, returns 0/1
 
 
 
 
 
+//C LIBRARY:
+//allocating memory:
+void *malloc(size_t s);	//allocates a block of memory of size s
+void *calloc(size_t n, size_t s);	//allocates n blocks of memory of size s
+
+//unallocating memory:
+void free(void *ptr);	//free block of memory at ptr
+
+//opens a file by file name and returns a file pointer to do stuff with
+FILE *fopen(const char *path, const char *mode);
+//opens a file by fd number and returns a file pointer to do stuff with
+FILE *fdopen(int fd, const char *mode);
+/*modes: 
+	r - open for reading (error if doesn't exist)
+	w - open for writing (creates if doesn't exist)
+	a - open for appending	(creates if doesn't exist)
+	r+ - open for reading and writing (error if doesn't exist)
+		 does not delete contents of file if it exists
+	w+ - open for writing and reading (creates if doesn't exist)
+		 overwrites the file if it exists
+	a+ - open for appending and reading (creates if doesn't exist)
+	*/
+
+//write to a stream/file
+int fprintf(FILE *fptr, const char *str, ...);
+//e.g write something to stdout:
+fprintf(1, "%d: %s %lf\n", 123, "this is a string", 3.1415);
+
+//read from a stream/file
+int fscanf(FILE *fptr, const char *str, ...);
+
+//get input from a stream/file, returns the number of bytes read
+size_t fread(void *buf, size_t size, size_t n, FILE *fptr);
+//output to a stream/file, returns the number of bytes written
+size_t fwrite(const void *buf, size_t size, size_t n, FILE *fptr);
+/*	buf - buffer to read/write input to
+	size - size of each element to read/write (e.g. char is 1, int is 4)
+	n - number of elements each with size bytes
+	fptr - input/output stream
+	*/
+
+
+
+
+
+
+
+
+//C PREPROCESSOR DIRECTIVES:
+#include <path>		//searches for path in system directories
+#include "path"		//searches for path in current directory
+
+#define MY_CONSTANT const_value_here	//replace MY_CONSTANT with const_value_here everywhere it is found
+#undefine MY_CONSTANT					//stops replacement of MY_CONSTANT
+
+#ifdef MY_CONSTANT		//do next code if MY_CONSTANT is defined
+#endif
+
+#ifndef MY_CONSTANT		//do next code if MY_CONSTANT is not defined
+#elif MY_OTHER_CONSTANT	//do next code if MY_OTHER_CONSTANT is defined instead
+#else					//do next code in all other cases
+#endif
