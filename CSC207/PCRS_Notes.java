0607016daa8b6df39cec42126afca96ba589a9c4
NyW8 <tobluee@gmail.com>
2019-09-07 07:57:26 -0400

    Added a few starting notes for CSC207

---
+++ CSC207/PCRS_Notes.java
@@ -0,0 +1,24 @@
+//Created by Nyah Way, for St. George's CSC207 class
+//This is a file with notes from the PCRS resource
+
+
+//Quest 1
+//Hello World
+//  - No code exists outside a class
+//	- There are no functions, but methods exists
+	public static void main(String[] args){}
+//	- when a class is run, this method is automatically called
+//	- public declares what has access to that object
+	System.out.println(5+7);
+//	- System is the class
+//	- out is a static member in the class
+//	- println is a method within that member
+//	- 5+7 is the expression
+
+
+//Variables
+	int i;						//default value for int is 0
+	Object obj;					//default value for any object is null
+	String name = "CSC207";		//note the capital 'S' and double quotes
+	boolean isTrue = false;		//lowercase boolean values (true/false)
+	double gpa = 0.0;
