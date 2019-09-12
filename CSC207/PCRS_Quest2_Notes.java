9224065dc0bc6fb6ec6c6549bb93fea94d7063b3
NyW8 <tobluee@gmail.com>
2019-09-11 23:20:34 -0400

    Started notes on Quest 2

---
+++ CSC207/PCRS_Quest2_Notes.java
@@ -0,0 +1,25 @@
+//Created by Nyah Way, for St. George's CSC207 class
+//This is a file with notes from the PCRS resource
+
+
+//Quest 2
+//Overloading
+//	- two methods that have the same name but different parameters
+//	- one can be a default, and the other can call it, or they can process inputs differently, etc
+//	- must be inside the same class
+//	- interfaces are clearer to work with
+//		- I DONT FULLY UNDERSTAND INTERFACE VS CLASS
+
+//Static
+//	- means you can have (for example) one variable that each instance of a class shares
+//	- make good counters, etc
+//	- kind of like global variables, but better
+//	- static variables can be inside any methods
+//	- regular variables CANNOT be inside static methods
+
+//toString and equals pt 1
+//	- toString has an default method already created for each object
+//		- can create your own toString method that overrides default
+//		- all object types have a toString method
+//	- equals has a default method already created for each object
+//		- like toString, can override default
